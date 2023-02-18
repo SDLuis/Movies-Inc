a20dbc68ab231367c16cd80869a980e473d3202d
Luis <Luistejeda0011@gmail.com>
2023-02-18 16:09:11 -0400

    getSimilarMovies fuction working

--- src/hooks/useSingleMovie.jsx
+++ src/hooks/useSingleMovie.jsx
@@ -1,38 +1,49 @@
 import { useState, useEffect, useRef } from 'react';
+import { toast } from 'react-hot-toast';
 import {
   getSingleMovie,
   getCreditsMovie,
   rateMovie,
+  getSimilarMovies
 } from '../services/movies.service';
 import { useParams } from 'react-router-dom';
 
 export default function useSingleMovie() {
   const params = useParams();
   const ID = params.ID;
+
   const [singleMovie, setSingleMovie] = useState([]);
   const [credits, setCredits] = useState([]);
+  const [similarMovies, setSimilarMovies] = useState([]);
+
   const [errorHandler, setErrorHandler] = useState(null);
-  const rateInput = useRef(null);
 
   useEffect(() => {
     getSingleMovie({ ID })
       .then(({ data }) => setSingleMovie(data))
       .catch((err) => setErrorHandler(err));
-  }, []);
+  }, [ID]);
 
   useEffect(() => {
     getCreditsMovie({ ID })
       .then(({ data }) => setCredits(data))
       .catch((err) => setErrorHandler(err));
-  }, []);
+  }, [ID]);
 
-  const RateMovie = () => {
-    const rate = parseFloat(rateInput.current?.value);
-    rateInput.current.value
-      ? rateMovie({ ID, rate }).then((res) => {
-          return res;
-        })
+  useEffect(() => {
+    getSimilarMovies({ ID })
+      .then(({ data }) => setSimilarMovies(data))
+      .catch((err) => setErrorHandler(err));
+  }, [ID]);
+
+  const RateMovie = ({ rate = null }) => {
+    rate
+      ? rateMovie({ ID, rate })
+          .then((res) => {
+            toast.success(res?.data?.status_message);
+          })
+          .catch((err) => toast.error(err))
       : null;
   };
-  return { singleMovie, credits, RateMovie, rateInput, errorHandler };
+  return { singleMovie, credits, RateMovie, errorHandler, similarMovies };
 }
