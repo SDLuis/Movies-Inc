c8e946eb9f707ab08e63c5f5614eab876d584d89
Luis <Luistejeda0011@gmail.com>
2023-02-18 19:50:10 -0400

    User img placeholder

--- src/home/details.jsx
+++ src/home/details.jsx
@@ -5,7 +5,12 @@ export default function Details() {
   const { singleMovie, credits, RateMovie, rateInput } = useSigleMovie();
   return (
     <div className='w-full min-h-screen flex justify-center items-center bg-[#121212] text-[#f2f2f2]'>
-      <MovieDetails movie={singleMovie} credits={credits} RateMovie={RateMovie} rateInput={rateInput} />
+      <MovieDetails
+        movie={singleMovie}
+        credits={credits}
+        RateMovie={RateMovie}
+        rateInput={rateInput}
+      />
     </div>
   );
 }
--- src/hooks/useMovies.jsx
+++ src/hooks/useMovies.jsx
@@ -1,14 +1,15 @@
-import { useState, useEffect} from 'react'
-import { getNowPlayingMovies } from '../services/movies.service'
+import { useState, useEffect } from 'react';
+import { getNowPlayingMovies } from '../services/movies.service';
 
 export default function useMovies() {
-
-  const [nowPlayingMovies, setNowPlayingMovies] = useState([])
-  const [ errorHandler, setErrorHandler] = useState(null)
+  const [nowPlayingMovies, setNowPlayingMovies] = useState([]);
+  const [errorHandler, setErrorHandler] = useState(null);
 
   useEffect(() => {
-    getNowPlayingMovies().then(({ data }) => setNowPlayingMovies(data)).catch((err) => setErrorHandler(err))
-  },[])
+    getNowPlayingMovies()
+      .then(({ data }) => setNowPlayingMovies(data))
+      .catch((err) => setErrorHandler(err));
+  }, []);
 
-  return { nowPlayingMovies }
+  return { nowPlayingMovies };
 }
